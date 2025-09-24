e2e1fdf97643b393e7ce468f7ee975a377710823
hamza1773 <hamzamohammadshah@gmail.com>
2025-09-23 22:10:03 -0400

    Initial Immersive Ads demo added

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { createClient } from '@/utils/supabase/server'
+import { createClient } from '../../../utils/supabase/server'
 import { notFound } from 'next/navigation'
 import ProfileClientPage from './ProfileClientPage' // Import the new client component
 
